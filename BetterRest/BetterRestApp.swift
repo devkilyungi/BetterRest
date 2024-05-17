a302b0c0fde8ca2ef7ec2194905716d39ddcf024
devkilyungi <victorkilyungi@gmail.com>
2024-05-17 12:09:02 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Victor Kilyungi on 17/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
